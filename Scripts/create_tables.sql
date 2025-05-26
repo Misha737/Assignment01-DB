4ea3b81a2ccc9cde237c604d6d98c386ee6d0aa3
Misha <makovkamisha10@gmail.com>
2025-05-26 13:14:10 +0300

    Creating of tables

--- Scripts/create_tables.sql
+++ Scripts/create_tables.sql
@@ -1,1 +1,34 @@
 create database a01;
+
+use a01;
+
+create table customers (
+	id int primary key auto_increment,
+	name varchar(50) not null,
+	birthday date not null,
+	good_id int not null,
+	purchase_id int not null
+);
+
+create table employees (
+	id int primary key auto_increment,
+	name varchar(50) not null,
+	birthday date not null
+);
+
+create table goods (
+	id int primary key auto_increment,
+	name varchar(50) not null
+);
+
+create table purchases (
+	id int primary key auto_increment,
+	date_of_purchase date not null,
+	good_id int null,
+	customer_id int not null
+);
+
+create table premises (
+	id int primary key auto_increment,
+	location varchar(50) not null
+);
